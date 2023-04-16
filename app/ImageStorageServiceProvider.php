aa57bbb83f9e8f9b20e06cd919108f3ba34639ac
andrii-rusanovych <a.dovgyj@gmail.com>
2023-04-16 13:35:57 +0300

    init + ui bootstrap --auth, modify .env.example change APP_NAME and chnageMAIL_MAILER to 'log'

---
+++ app/ImageStorageServiceProvider.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App;
+
+use Illuminate\Support\ServiceProvider;
+
+class ImageStorageServiceProvider extends ServiceProvider
+{
+    public function register(): void
+    {
+
+    }
+
+    public function boot(): void
+    {
+    }
+}
